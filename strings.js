f45845a5875dcf8aec98c9ed75f32603ccfcc2c0
avery2002 <123275379+avery2002@users.noreply.github.com>
2023-12-07 17:55:14 -0700

    Add files via upload

--- strings.js
+++ strings.js
@@ -1,38 +1,34 @@
-//This function takes in the user's inputs and returns message/alerts depending on their input
-function getInput() {
+    function validateInputs() {
+        // Get values from input fields in the webpage
+        var fname = document.getElementById("fname").value;
+        var lname = document.getElementById("lname").value;
+        var zip = document.getElementById("zip").value;
 
- //This begins the loop
-do{
-    //This takes in the user's first and last name inputs and stores them in variables
-    fName = prompt("First Name: ");
-    lName = prompt("Last name: ");
-    
-    //This variable stores the user's combined first and last names
-    fullName = fName + " " + lName;
+        // Combine first name and last name into one variable
+        var fullName = fname + " " + lname;
 
-    //This begins checking if the user's name is longer than 20 characters
-    if(fullName.length > 20) {
-        //This displays an alert if the user's name is longer than 20 characters
-        alert("Name too large.");
-    }
-//This breaks the loop involving the fullName length
-} while(fullName.length > 20)
-
-    do {
-        //This variable stores the zip code the user inputs
-        zip = prompt("Zip Code: ");
-
-        //This begins a loop that checks if the user's zip input is 5 digits
-        if(zip.length != 5) {
-            //This displays an alert if the user's zip input is not 5 digits
-            alert("Please input a valid zip code.");
+        // Check if the combined name has more than 20 characters
+        if (fullName.length > 20) {
+            // Display alert if too large
+            alert("name too large");
+            // Stop execution if invalid
+            return;
         }
 
-    //This breaks the loop involving the zip code length
-    } while(zip.length !=5) {
-        //This displays the secret message once all the requirements have been passed
-        alert("Hooray!");
+        // Check if the zip code contains exactly 5 digits
+        if (!/^\d{5}$/.test(zip)) {
+            // Display alert if incorrect number of digits
+            alert("Invalid zip. Zip should be 5 digits.");
+            // Stop execution if invalid
+            return; 
+        }
+
+        // If iall inputs are coorect, display the secret message
+        alert("You found the secret message, " + fullName + "!");
     }
 
 
-}
+
+
+
+
